8389af307e83c4d59443356304bc607699b91d10
valkyrienyanko <sebastianbelle074@gmail.com>
2018-11-12 14:15:34 -0600

    any block

--- Tiles/Platform.cs
+++ Tiles/Platform.cs
@@ -7,28 +7,29 @@ namespace PlatformHelper.Tiles
 {
     public class Platform : GlobalTile
     {
-        public override void PlaceInWorld(int i, int j, Item item) {
-            if (PlatformHelper.PlatformHotKey.Current) {
-                if (item.createTile == TileID.Platforms)
+        public override void PlaceInWorld(int i, int j, Item item)
+        {
+            if (PlatformHelper.PlatformHotKey.Current)
+            {
+                int amount = Math.Min(Main.LocalPlayer.HeldItem.stack, 5);
+                Item heldItem = Main.LocalPlayer.HeldItem;
+                if (Main.LocalPlayer.direction == 1)
                 {
-                    int amount = Math.Min(Main.LocalPlayer.HeldItem.stack, 5);
-                    Item heldItem = Main.LocalPlayer.HeldItem;
-                    if (Main.LocalPlayer.direction == 1)
+                    heldItem.stack -= (amount - 1);
+                    for (int n = 1; n < amount; n++)
                     {
-                        heldItem.stack -= (amount - 1);
-                        for (int n = 1; n < amount; n++)
-                        {
-                            WorldGen.PlaceTile(i + n, j, item.createTile, style:item.placeStyle);
-                        }
-
+                        WorldGen.PlaceTile(i + n, j, item.createTile, style: item.placeStyle);
+                        NetMessage.SendTileSquare(Main.LocalPlayer.whoAmI, i + n, j, 1);
                     }
-                    else
+
+                }
+                else
+                {
+                    heldItem.stack -= (amount - 1);
+                    for (int n = 1; n < amount; n++)
                     {
-                        heldItem.stack -= (amount - 1);
-                        for (int n = 1; n < amount; n++)
-                        {
-                            WorldGen.PlaceTile(i - n, j, item.createTile, style:item.placeStyle);
-                        }
+                        WorldGen.PlaceTile(i - n, j, item.createTile, style: item.placeStyle);
+                        NetMessage.SendTileSquare(Main.LocalPlayer.whoAmI, i - n, j, 1);
                     }
                 }
             }
